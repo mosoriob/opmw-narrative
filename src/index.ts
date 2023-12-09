e4b21c192bedb0885d5b80820c87977f6aa91443
Maximiliano Osorio <maxiosorio@gmail.com>
2023-12-09 09:42:07 -0300

    safe: rdf-deference

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+import { get } from "http";
+import rdfDereferencer from "rdf-dereference";
+import { storeStream } from "rdf-store-stream";
+
+(async () => {
+  const { data: stream } = await rdfDereferencer.dereference(
+    "https://opmw.org/exportTest/resource/WorkflowExecutionAccount/SpectralAnalysis-2edd5b01-efae-483b-8fc3-c8a8794f18bd"
+  );
+  const store = await storeStream(stream);
+  console.log(store);
+})();
